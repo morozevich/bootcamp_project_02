dfc01886bdf75bd40db110f394ec385fdbd8a774
cmaquino <aquinoqcd29@gmail.com>
2022-11-06 23:12:43 -0300

    Add GithubHeadersCache.

--- lib/github/repos/starred_repos/infrastructure/starred_repos_remote_service.dart
+++ lib/github/repos/starred_repos/infrastructure/starred_repos_remote_service.dart
@@ -1,11 +1,14 @@
 import 'package:bootcamp_project_02/core/infraestructure/remote_response.dart';
+import 'package:bootcamp_project_02/github/core/infrastructure/github_headers.dart';
 import 'package:bootcamp_project_02/github/core/infrastructure/github_repo_dto.dart';
+import 'package:bootcamp_project_02/github/core/infrastructure/githuh_headers_cache.dart';
 import 'package:dio/dio.dart';
 
 class StarredReposRemoteService {
   final Dio _dio;
+  final GithuhHeadersCache _githuhHeadersCache;
 
-  StarredReposRemoteService(this._dio);
+  StarredReposRemoteService(this._dio, this._githuhHeadersCache);
 
   Future<RemoteResponse<List<GithubRepoDTO>>?> getRepoStarrePage(
     int page,
